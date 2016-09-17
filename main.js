6c925a7e0e82e8dd73a7a1e13e0ecc5d8f805253
Olga Sizova <Olga@Grigorys-MacBook-Air.local>
2016-09-17 18:14:01 -0400

    added game logic

---
+++ main.js
@@ -0,0 +1,24 @@
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+
+
+if (cardOne === cardTwo){
+	alert('You found a match!');
+}
+
+if (cardTwo === cardOne){
+  alert('Youf found a match!');
+}
+
+if (cardThree === cardFour){
+  alert('You found a match!');
+}
+
+if (cardFour === cardThree){
+  alert('You found a match!');
+}
+
+else alert('Try again!');
